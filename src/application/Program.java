62d29e5b842dee76a8cb31a1186d2ca2bc85283b
eduardo andré <you@example.com>
2022-04-22 16:14:45 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactor;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -11,6 +13,9 @@ public class Program {
 		
 		Department obj = new Department(1, "Books");
 		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+		
+		new DaoFactor();
+		SellerDao sellerDao = DaoFactor.createSellerDao();
 				
 		System.out.println(seller);
 	}
