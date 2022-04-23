29d4eed750d5f2e3b8fe96f42f335096255aab2a
eduardo andré <you@example.com>
2022-04-23 15:07:55 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -19,7 +19,7 @@ public class Program {
 		System.out.println(seller);
 		
 		System.out.println("\n===TEST 2: seller findByDepartment===");
-		Department department = new Department(2, null);
+		Department department = new Department(3, null);
 		List<Seller> list = sellerDao.findByDepartment(department);
 		list.forEach(System.out::println);
 		
@@ -31,6 +31,15 @@ public class Program {
 		Seller Newseller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
 		sellerDao.insert(Newseller);
 		System.out.println("Inserted new id " + Newseller.getId());
+		
+		System.out.println("\n===TEST 5: seller update===");
+		seller = sellerDao.findById(9);
+		seller.setName("Martha Waine");
+		seller.setEmail("martha@gmail.com");
+		seller.setDepartment(department);
+		seller.setBaseSalary(4300.0);
+		sellerDao.update(seller);
+		System.out.println("Update completed!");
 	}
 
 }
